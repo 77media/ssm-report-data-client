06effce0049495d28cd640d54200f2209eeec4aa
Aaron Stanley King <trendoid@gmail.com>
2017-06-16 11:53:10 -0400

    added security token filter to POST

--- app/routes.js
+++ app/routes.js
@@ -10,10 +10,17 @@ module.exports = function (app, io, socket) {
 
     // post endpoint for platform to send to -------------------------------------------------------------
     app.post('*', function (req, res) {
-        var message = req.body;
-        message.type = 'message';
-        message.created = Date.now();
-        io.sockets.emit('message', req.body);
-        res.send(200, 'thanks');
+        //security method token
+        if (req.body.token && req.body.token === 'f7e1379c-35ad-45f9-b0fa-6ef011e73938') {
+            // making message a bit better for the socket
+            var message = req.body;
+            message.type = 'message';
+            message.created = Date.now();
+            io.sockets.emit('message', message);
+            res.send(200, 'thanks');
+        }
+        else {
+            res.send(400);
+        }
     });
 };
